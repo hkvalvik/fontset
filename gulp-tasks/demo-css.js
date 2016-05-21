454bb51de8372434127c272b64a43bf534ae1f69
hkvalvik <administrator@Havards-MacBook-Pro.local>
2016-05-21 13:45:32 +0200

    Added autoprefixer to demo css

--- gulp-tasks/demo-css.js
+++ gulp-tasks/demo-css.js
@@ -1,4 +1,7 @@
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
 var concat = require('gulp-concat');
 
 module.exports = function(gulp){
@@ -7,8 +10,11 @@ module.exports = function(gulp){
                 'node_modules/highlight.js/styles/default.css',
                 'demo/index.scss'
             ])
+            .pipe(sourcemaps.init())
             .pipe(concat('index.min.css'))
             .pipe(sass().on('error', sass.logError))
+            .pipe(postcss([ autoprefixer({ browsers: ['last 3 versions', '> 1%'] }) ]))
+            .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest('demo/dist'));
     };
 };
